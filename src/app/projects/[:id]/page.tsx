a87d50f02d9a2a4343f30a0b4b36e61fe9322584
ADITYA RAJ <adityaraj6220@gmail.com>
2025-03-03 19:40:28 +0530

    bugs resolve 2

--- src/app/projects/[:id]/page.tsx
+++ src/app/projects/[:id]/page.tsx
@@ -4,7 +4,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLink } from "@fortawesome/free-solid-svg-icons";
 import { faSquareGithub } from "@fortawesome/free-brands-svg-icons";
 import projectdata from "../../../lib/project.json";
-import  PageProps  from "next";
 
 interface Props {
   searchParams: {
